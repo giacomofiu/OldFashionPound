42201ef1350487fd785c26d583992a219db0bead
Giacomo Fiumara <giacomof@MBP-di-Giacomo.fritz.box>
2021-02-04 18:49:21 +0100

    old fashion pound, excercise for euris

---
+++ src/main/java/giacomo/euris/oldfashionpound/OldMoney.java
@@ -0,0 +1,41 @@
+package giacomo.euris.oldfashionpound;
+
+public class OldMoney {
+
+	private Integer p;	//sterlina
+	private Integer s;	//scellino
+	private Integer d;	//penny
+	
+	public OldMoney() {}
+
+	public OldMoney(Integer p, Integer s, Integer d) {
+		this.p = p;
+		this.s = s;
+		this.d = d;
+	}
+
+	public Integer getP() {
+		return p;
+	}
+
+	public void setP(Integer p) {
+		this.p = p;
+	}
+
+	public Integer getS() {
+		return s;
+	}
+
+	public void setS(Integer s) {
+		this.s = s;
+	}
+
+	public Integer getD() {
+		return d;
+	}
+
+	public void setD(Integer d) {
+		this.d = d;
+	}
+
+}
---
+++ src/main/java/giacomo/euris/oldfashionpound/OperationsOldPoundImpl.java
@@ -0,0 +1,118 @@
+package giacomo.euris.oldfashionpound;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OperationsOldPoundImpl implements OperationsOldPound{
+
+	private final Integer D_MODULE = 12;
+	private final Integer S_MODULE = 20;
+	
+	public OldMoney sum(List<OldMoney> listMoney) {
+
+		OldMoney resultMoney = new OldMoney();
+		OldMoney price1 = listMoney.get(0);
+		OldMoney price2 = listMoney.get(1);
+		Integer d, s, p, carryOver;
+		carryOver = 0;
+		
+		d = price1.getD() + price2.getD();
+		resultMoney.setD(d % D_MODULE);
+		carryOver = d/D_MODULE;
+		
+		s = price1.getS() + price2.getS() + carryOver;
+		resultMoney.setS(s % S_MODULE);
+		
+		carryOver = s/S_MODULE;
+		
+		p = price1.getP() + price2.getP();
+		resultMoney.setP(p + carryOver);
+				
+		return resultMoney;
+	}
+
+	public OldMoney difference(List<OldMoney> listMoney) {
+		
+		OldMoney resultMoney = new OldMoney();
+		OldMoney price1 = listMoney.get(0);
+		OldMoney price2 = listMoney.get(1);
+		Integer d, s, p, carryOver;
+		
+		carryOver = 0;
+		
+		d = price1.getD() - price2.getD();
+		//System.out.println("d % 12 = " + (d%D_MODULE + D_MODULE)%D_MODULE );
+		resultMoney.setD((d%D_MODULE + D_MODULE) % D_MODULE);
+		if(d < 0) {
+			carryOver = -1;
+			carryOver = carryOver + (d/D_MODULE);
+		}else {
+			carryOver = d/D_MODULE;
+		}
+		
+		s = price1.getS() - price2.getS() + carryOver;
+		resultMoney.setS((s%S_MODULE + S_MODULE) % S_MODULE);
+		
+		carryOver = 0;
+		if(s < 0) {
+			carryOver = -1;
+			carryOver = carryOver + (s/S_MODULE);
+		}else {
+			carryOver = s/S_MODULE;
+		}
+
+		p = price1.getP() - price2.getP();
+		resultMoney.setP(p + carryOver);
+				
+		return resultMoney;
+	}
+
+	
+	public OldMoney multiplication(OldMoney price, Integer n) {
+		
+		OldMoney result = new OldMoney();
+		Integer d, s, p, carryOver;
+		
+		d = price.getD() * n;
+		result.setD(d % D_MODULE);
+		carryOver = d/D_MODULE;
+		
+		s = (price.getS() * n) + carryOver;
+		result.setS(s % S_MODULE);
+		carryOver = s / S_MODULE;
+		
+		p = (price.getP() * n) + carryOver;
+		result.setP(p);
+		
+		return result;
+	}
+
+	public List<OldMoney> division(OldMoney price, Integer n) {
+		
+		OldMoney result = new OldMoney();
+		OldMoney remainder = new OldMoney();
+		List<OldMoney> listResults = new ArrayList<OldMoney>();
+		Integer d, s, p, carryOver;
+		
+		
+		d = price.getD() / n;
+		result.setD(d % D_MODULE);
+		remainder.setD(price.getD() % n);
+		carryOver = d / D_MODULE;
+		
+		s = (price.getS() / n) + carryOver;
+		result.setS(s % S_MODULE);
+		remainder.setS( (price.getS() % n));
+		carryOver = s / S_MODULE;
+		
+		p = (price.getP() / n) + carryOver;
+		result.setP(p);
+		remainder.setP(price.getP() % n); 
+		
+		listResults.add(result);
+		listResults.add(remainder);
+		
+		return listResults;
+	}
+
+}
